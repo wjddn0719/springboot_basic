1ad2aaa1149e0f653d8565964a957cbdaee20278
jungwoo0719 <jungwoo07191@naver.com>
2025-11-20 23:43:56 +0900

    funny spring

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,13 +3,27 @@ package hello.hello_spring.controller;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public String Hello(Model model) {
-        model.addAttribute("data", "hello!");
+        model.addAttribute("name", "정우");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-mvc";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name, Model model){
+        return "hello " + name;
     }
 }
